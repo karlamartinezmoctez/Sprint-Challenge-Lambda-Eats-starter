b7b408b8927f9215c1fcd4ef67d0329995f9ca03
karlamartinezmoctez <karlamartinezmoctez@flpoly.org>
2020-06-12 23:48:41 -0400

    home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
+//2. import {Route}
+import {Route} from "react-router-dom"
+//5. import the remainding libraries
+import {Link} from "react-router-dom"
+//6. import reacstrap components
+import {Button, Navbar, Card, CardImg, ButtonGroup} from "reactstrap"
+
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Navbar>
+      <h1>LAMBDA EATS</h1> 
+
+      <ButtonGroup>
+        <Link To="/">
+      <Button outline color="primary" color="info" align="right">Home</Button> 
+      </Link>
+      <Button outline color="primary" color="info">Help</Button> 
+      </ButtonGroup>
+
+    {/* 7. adding the header image */}
+    <Card>
+      <CardImg src={require("../Assets/Pizza.jpg")}/>
+    </Card>
+ 
+    
+    </Navbar>
+
+    
+
+
+    {/* 4. Render the menu */}
+    
+    
+      </>
   );
 };
 export default App;
-//comment
--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+//1. import Browser Router
+import { BrowserRouter as Router } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<Router>
+<App />
+</Router>
+, document.getElementById("root"));
